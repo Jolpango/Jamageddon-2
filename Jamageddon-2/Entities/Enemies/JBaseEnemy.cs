aafcd8db7bed8eb6f3c5470a6014a0ce7afe46e8
osiv-me <erik.erlandsson.mail@gmail.com>
2025-06-06 20:58:02 +0200

    wip

--- Jamageddon-2/Entities/Enemies/JBaseEnemy.cs
+++ Jamageddon-2/Entities/Enemies/JBaseEnemy.cs
@@ -15,7 +15,7 @@ namespace Jamageddon2.Entities.Enemies
         public float Health { get; protected set; }
         public float MaxHealth { get; protected set; }
         public float MoveSpeed { get; protected set; }
-        public string Name { get; protected set; }
+        public new string Name { get; protected set; }
         public bool IsAlive => Health > 0;
         public bool IsMoving => this.GetComponent<JPathInputComponent>() != null && this.GetComponent<JPathInputComponent>().IsMoving;
 
@@ -30,27 +30,19 @@ namespace Jamageddon2.Entities.Enemies
             Tags = new HashSet<string> { "Enemy" };
 
             // Add required components
-            var transformComponent = new JTransformComponent();
-            var spriteComponent = new JSpriteComponent(spritePath);
-            var colliderComponent = new JColliderComponent()
+            AddComponent(new JHealthComponent(maxHealth));
+            AddComponent(new JSpriteComponent(spritePath));
+            AddComponent(new JColliderComponent()
             {
                 Size = new Vector2(32, 32),
                 IsSolid = false
-            };
-            var movementComponent = new JMovementComponent() { Speed = moveSpeed };
-            var HealthComponent = new JHealthComponent(maxHealth);
-            var pathInputComponent = new JPathInputComponent();
+            });
+            AddComponent(new JMovementComponent() { Speed = moveSpeed });
+            AddComponent(new JTransformComponent());
+            AddComponent(new JPathInputComponent());
 
-            AddComponent(HealthComponent);
-            AddComponent(spriteComponent);
-
-            AddComponent(movementComponent);
-            AddComponent(transformComponent);
-            AddComponent(colliderComponent);
-            AddComponent(pathInputComponent);
-
-            pathInputComponent.OnPathComplete += OnPathComplete;
-            HealthComponent.OnDeath += OnDeath;
+            this.GetComponent<JPathInputComponent>().OnPathComplete += OnPathComplete;
+            this.GetComponent<JHealthComponent>().OnDeath += OnDeath;
         }
 
         public void SetPath(JPathComponent path)
